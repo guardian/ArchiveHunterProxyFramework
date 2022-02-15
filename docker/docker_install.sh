e62d04e923508aa038308c6aefbd8861816cfc44
andy_gallagher <andy.gallagher@theguardian.com>
2022-02-15 17:17:39 +0000

    Fix image build - new ffmpeg installation, security improvements, remove insecure repo

--- docker/docker_install.sh
+++ docker/docker_install.sh
@@ -8,5 +8,21 @@ wget https://www.dechifro.org/dcraw/archive/dcraw-9.28.0.tar.gz
 tar xvzf dcraw-9.28.0.tar.gz
 (cd /dcraw && bash ./install)
 (cd /dcraw && gcc -o dcraw -O4 dcraw.c -lm -ljasper -ljpeg -llcms2)
+
+wget https://cdn.theguardian.tv/ffmpeg-release-amd64-static.tar.xz
+wget https://cdn.theguardian.tv/ffmpeg-release-amd64-static.tar.xz.md5
+md5sum ffmpeg-release-amd64-static.tar.xz > downloaded-md5.txt
+diff downloaded-md5.txt ffmpeg-release-amd64-static.tar.xz.md5
+
+if [ "$?" != "0" ]; then
+  echo ffmpeg binary failed checksum
+  exit 1
+fi
+xz -dc ffmpeg-release-amd64-static.tar.xz | tar xv
+/usr/bin/install ffmpeg-5.0-amd64-static/ffmpeg /usr/local/bin
+/usr/bin/install ffmpeg-5.0-amd64-static/ffprobe /usr/local/bin
+/usr/bin/install ffmpeg-5.0-amd64-static/qt-faststart /usr/local/bin
+rm -rf ffmpeg-5.0-amd64-static
+rm -f ffmpeg-release-amd64-static.tar.xz ffmpeg-release-amd64-static.tar.xz.md5 downloaded-md5.txt
 yum -y groupremove 'Development Tools'
 yum -y remove wget
